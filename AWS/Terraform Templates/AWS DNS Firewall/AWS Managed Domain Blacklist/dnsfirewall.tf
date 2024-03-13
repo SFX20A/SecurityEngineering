483cbcd5a8820f8859758bcc1fafe5cb2e7fd6ba
SFX20A <jianzhong90@gmail.com>
2024-03-13 17:36:36 +0800

    Update AWS Managed DNS Firewall Rules

--- AWS/Terraform Templates/AWS DNS Firewall/AWS Managed Domain Blacklist/dnsfirewall.tf
+++ AWS/Terraform Templates/AWS DNS Firewall/AWS Managed Domain Blacklist/dnsfirewall.tf
@@ -1,21 +1,16 @@
-resource "aws_route53_resolver_firewall_domain_list" "blacklist_domains" {
-  name    = "Blacklisted_Domains"
-  domains = var.blacklist_domains
-}
-
 resource "aws_route53_resolver_firewall_rule_group" "blacklist_domains_rule_group" {
-  name = "blacklist_domain_rule_group"
+  name = "managed_list_rule_group"
 }
 
 resource "aws_route53_resolver_firewall_rule" "blacklist_domains_rule" {
-  name                    = "dns_firewall_rule"
+  count                   = length(var.managed_lists)
+  name                    = "${var.managed_lists[count.index]}-dns_firewall_rule"
   action                  = "BLOCK"
   block_response          = "NXDOMAIN" # Edit according based on block response, resource block is written based on NODATA or NXDOMAIN
-  firewall_domain_list_id = aws_route53_resolver_firewall_domain_list.blacklist_domains.id
+  firewall_domain_list_id = var.managed_lists[count.index]
   firewall_rule_group_id  = aws_route53_resolver_firewall_rule_group.blacklist_domains_rule_group.id
-  priority                = var.priority
+  priority                = var.priority + count.index
 }
-
 
 resource "aws_route53_resolver_firewall_rule_group_association" "blacklist_domains_association" {
   for_each               = var.vpc_id
--- AWS/Terraform Templates/AWS DNS Firewall/AWS Managed Domain Blacklist/variables.tf
+++ AWS/Terraform Templates/AWS DNS Firewall/AWS Managed Domain Blacklist/variables.tf
@@ -1,19 +1,22 @@
-
 variable "blacklist_domains" {
   description = "Blacklisted Domains"
   type        = list(any)
   default     = ["example1.com", "example2.com", "example3.com"] # Add * if you want to block subdomains via wildcards.
 }
 
-# Note that priority has to be between 100 and 9900
 variable "priority" {
   description = "Indicate rule priority"
   type        = number
 }
 
-# Indicate all VPC IDs 
 variable "vpc_id" {
   description = "Indicate VPC ID to apply DNS Firewall rules"
   type        = set(string)
 }
 
+variable "managed_lists" {
+  description = "List of AWS Managed DNS Firewall rules"
+  type        = list(string)
+  default     = ["rslvr-fdl-49099fd7fc3d4853", "rslvr-fdl-b1c1d04c32194010", "rslvr-fdl-2ff056c2bf334345", "rslvr-fdl-cc92cf0f3482427c"] # IDs of AWS Managed DNS Firewall Rules
+
+}
